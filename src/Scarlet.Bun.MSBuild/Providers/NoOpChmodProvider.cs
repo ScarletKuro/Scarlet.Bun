3aeaad39bb73fe54a18648856f33f1ac7f8e23a3
ScarletKuro <artem.melnikov@live.com>
2026-02-15 18:17:04 +0200

    use internal

--- src/Scarlet.Bun.MSBuild/Providers/NoOpChmodProvider.cs
+++ src/Scarlet.Bun.MSBuild/Providers/NoOpChmodProvider.cs
@@ -7,7 +7,7 @@
 /// This provider is used on platforms where executable permissions are
 /// either not applicable or are managed by the operating system (e.g. Windows).
 /// </remarks>
-public sealed class NoOpChmodProvider : IChmodProvider
+internal sealed class NoOpChmodProvider : IChmodProvider
 {
     /// <inheritdoc />
     public void EnsureExecutablePermissions(string filePath)
